fd874a3337c6e8f913f633bf8b19cd0ce74a56c3
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-10-15 14:16:34 +0500

    setting the server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Express Server Running")
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+// creating an app
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+// route for contacts
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json({ message: "Get all the Contacts" })
+})
+
+// listening on port
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`)
+})
